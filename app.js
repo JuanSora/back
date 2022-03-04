78bc55146e9035ea20d2a0d93b445555747f2b11
Ion Sora <soraioniosif@gmail.com>
2022-03-04 20:10:22 +0100

    edit app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express =require ('express')
-const app= express()
-const port= process.env.PORT || 8001
+//***express***
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8001;
 
-app.get('/lists',(req,res,next)=>{
-    res.json('hello World')
-})
+app.get("/lists", (req, res, next) => {
+  res.json({
+      "hello World",
+      'env':process.env.MSG ||'noexiste',
+      'port':port
+  })
+});
 
-
-app.listen(port, ()=>{
-    console.log(`Server listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
